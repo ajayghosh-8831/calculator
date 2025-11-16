d60ce30b8595e042b4770f194558236462a11d9a
AJAYGHOSH <ajayghosh526@gmail.com>
2025-11-16 17:54:29 +0530

    Unit Test changes

--- src/app/probability.service.spec.ts
+++ src/app/probability.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { ProbabilityService } from './probability.service';
 
@@ -6,7 +7,9 @@ describe('ProbabilityService', () => {
   let service: ProbabilityService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
     service = TestBed.inject(ProbabilityService);
   });
 
